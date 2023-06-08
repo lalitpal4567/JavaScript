e0b1160873347db910fef65e9e8becf41f6c0b22
Lalit Pal <103810580+lalitpal4567@users.noreply.github.com>
2023-06-08 17:46:59 +0530

    added files

---
+++ dataTypes/script.js
@@ -0,0 +1,21 @@
+// data types: 
+// 1. Number
+// 2. String
+// 3. Boolean
+
+// 4. Undefined
+// 5. Null
+// 6. BigInt
+
+let age = 23; // number
+let mood = "happy"; // string
+let isFun = true;   // boolean
+let dob;    // undefined
+let height = null;
+
+console.log(age+"   "+typeof age);
+console.log(mood+"   "+typeof mood);
+console.log(isFun+"   "+typeof isFun);
+console.log(dob+"   "+typeof dob);
+console.log(height+"   "+typeof height);    // bug
+console.log(typeof null);   // bug
